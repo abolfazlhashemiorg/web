5efaf5fdb0e34532d59935174a76921e3e0c8610
intel <abolfazlhashemiorg@gmail.com>
2026-07-08 15:38:16 +0330

    commit end

--- mobile_shop/shop/views.py
+++ mobile_shop/shop/views.py
@@ -40,17 +40,34 @@ def cart_view(request):
     cart = None
     if request.user.is_authenticated:
         cart, created = Cart.objects.get_or_create(user=request.user)
+    else:
+        if not request.session.session_key:
+            request.session.create()
+        session_key = request.session.session_key
+        cart, created = Cart.objects.get_or_create(session_key=session_key)
+    
     return render(request, 'cart.html', {'cart': cart})
 
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    cart, created = Cart.objects.get_or_create(user=request.user)
+    
+    # اگر کاربر لاگین هست، از user استفاده کن
+    if request.user.is_authenticated:
+        cart, created = Cart.objects.get_or_create(user=request.user)
+    else:
+        # اگر کاربر ناشناسه، از session_key استفاده کن
+        if not request.session.session_key:
+            request.session.create()
+        session_key = request.session.session_key
+        cart, created = Cart.objects.get_or_create(session_key=session_key)
+    
+    # اضافه کردن آیتم به سبد خرید
     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
     if not created:
         cart_item.quantity += 1
         cart_item.save()
+    
     return redirect('cart')
-
 def checkout(request):
     return render(request, 'checkout.html')
 
